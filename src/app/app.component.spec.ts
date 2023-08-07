874b01cbcf5c7bff2fe2415975a44cb5d1ce2bf9
FRpeemapot <freezeosiris@gmail.com>
2023-08-07 18:43:53 +0700

    week6

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-ng3' title`, () => {
+  it(`should have the 'my-ng' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-ng3');
+    expect(app.title).toEqual('my-ng');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-ng3 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('my-ng app is running!');
   });
 });
